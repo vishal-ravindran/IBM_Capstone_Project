53bdacd67af5b72eeeb67e617ed97967d67b8815
vishal ravindran <vishalravindran.student@gmail.com>
2024-05-09 14:55:23 -0400

    undeleted code

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,29 +1,55 @@
-# Uncomment the following imports before adding the REST API code
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from .models import CarMake, CarModel
+# Uncomment the imports below before you add the function code
+import requests
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-# @csrf_exempt
-# def car_make_list(request):
-#     # Add code to return a list of car makes
-@csrf_exempt
-def car_make_list(request):
-    # Add code to return a list of car makes
+backend_url = os.getenv(
+    'backend_url', default="http://localhost:3030")
+sentiment_analyzer_url = os.getenv(
+    'sentiment_analyzer_url',
+    default="http://localhost:5050/")
+
+# def get_request(endpoint, **kwargs):
+# Add code for get requests to back end
+def get_request(endpoint, **kwargs):
+    params = ""
+    if(kwargs):
+        for key,value in kwargs.items():
+            params=params+key+"="+value+"&"
+
+    request_url = backend_url+endpoint+"?"+params
+
+    print("GET from {} ".format(request_url))
     try:
-        car_makes = CarMake.objects.all()
-        data = {"car_makes": list(car_makes.values())}
-        return JsonResponse(data)
-    except Exception as e:
-        return JsonResponse({"error": str(e)}, status=500)
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        print(f"Response : {response}")
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
 
-
-@csrf_exempt
-def car_model_list(request, make_id):
-    # Add code to return a list of car models for a given car make id
+# def analyze_review_sentiments(text):
+# request_url = sentiment_analyzer_url+"analyze/"+text
+# Add code for retrieving sentiments
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+"analyze/"+text
     try:
-        car_models = CarModel.objects.filter(car_make_id=make_id)
-        data = {"car_models": list(car_models.values())}
-        return JsonResponse(data)
-    except Exception as e:
-        return JsonResponse({"error": str(e)}, status=500)
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
+
+# def post_review(data_dict):
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
