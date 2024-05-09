a5abecee6df5ef6d125cb6b37a3e0b36b239f1f3
vishal ravindran <vishalravindran.student@gmail.com>
2024-05-09 13:48:03 -0400

    lint correction models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,11 +4,12 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    
+        
     def __str__(self):
         return self.name
 
